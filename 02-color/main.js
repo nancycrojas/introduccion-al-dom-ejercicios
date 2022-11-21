6f0bd2e7f092799cad30c7612715e74bada8409a
nancycrojas <nanclarisaok@gmail.com>
2022-11-21 00:35:06 -0300

    Elimino ejercicios del 1 al 6

--- 02-color/main.js
+++
@@ -1,7 +0,0 @@
-//color
-//Hacer un programa que al iniciarse pida mediante un prompt ingresar un color en sistema hexadecimal, y una vez ingresado ponga el color de fondo del body con dicho color.
-
-let $ = (selector) => document.querySelector(selector);
-let color= prompt('Ingrese un color en sistema hexadecimal. Ej: #FF9900 Puede incluir números del 0 al 9 o letras de la A a la F ');
-let body=$('body');
-body.style.backgroundColor = color;
--- 03-rgb/main.js
+++
@@ -1,9 +0,0 @@
-//rgb
-//Hacer un programa que al iniciarse pida mediante tres prompts ingresar los valores correspondientes a un color en sistema rgb, y una vez ingresado ponga el color de fondo del body con dicho color. Los valores van del 0 al 255 y corresponden a rojo, verde y azul.
-
-let $ = (selector) => document.querySelector(selector);
-let color1 = prompt('Ingrese un número de 0 a 255');
-let color2 = prompt('Ingrese un número de 0 a 255');
-let color3 = prompt('Ingrese un número de 0 a 255');
-let body=$('body');
-body.style.backgroundColor = `rgb(${color1} , ${color2} , ${color3})`;
